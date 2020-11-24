d9602a68ad344c626138f4cbaad73ef9aabb6522
harmanwho <harmandeepsinghful@gmail.com>
2020-11-23 17:47:10 -0800

    Heroku experiment env

--- db/jobmongodb.js
+++ db/jobmongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectId } = require("mongodb");
 // const dotenv = require("dotenv");
 // dotenv.config();
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+// if (process.env.NODE_ENV !== "production") require("dotenv").config();
 function myDB() {
 	const myDB = {};
 	const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}`;
